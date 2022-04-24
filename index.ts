9219c83e53e6aacd90a71ca769593cbfcc893f01
Nico Kunz <nicokunznk00@gmail.com>
2022-04-24 22:59:04 +0200

    Add documentation to functions and refactor

--- index.ts
+++ index.ts
@@ -10,7 +10,9 @@ const URL_AUTHORNAME = 'https://tubiblio.ulb.tu-darmstadt.de/cgi/search/archive/
 const DATA_FOLDER = 'data/'
 
 app.get('/update', (req: Request, res: Response) => {
-    downloadJSONFile(URL_ALL, DATA_FOLDER + "all.json").then(() => res.sendStatus(200)).catch((err) => res.send(err));
+    downloadJSONFile(URL_ALL, DATA_FOLDER + "all.json")
+        .then(() => res.sendStatus(200))
+        .catch((err) => res.send(err));
 });
 
 app.get('/all', (req: Request, res: Response) => {
@@ -20,9 +22,9 @@ app.get('/all', (req: Request, res: Response) => {
             else res.json(JSON.parse(data.toString()));
         } catch (error) {
             console.error(error);
-            res.status(500).send((error as Error).message)
+            res.status(500).send((error as Error).message);
         }
-    })
+    });
 });
 
 app.get('/test', (req: Request, res: Response) => {
@@ -48,22 +50,32 @@ app.listen(
     () => console.log(`It's alive on http://localhost:${PORT}`)
 );
 
+/**
+ * Get JSON file content, if JSON file doesn't exist download it and write to disk
+ * @param {String} url The URL from which to download the desired JSON file
+ * @param {String} path The filepath where the desired JSON file should be
+ * @returns The content of the desired JSON file as JSON
+ */
 const getJSONFile = (async (url: string, path: string) => {
-    //return fs.promises.readFile(path).then(res => res.toString()).catch(err => downloadJSONFile(url, path))
     let data;
     
     try {
         data = await fs.promises.readFile(path);
         data = JSON.parse(data.toString());
     } catch (error) {
+        // download file if it couldn't be found
         data = await downloadJSONFile(url, path);
     }   
 
     return data;
-
 });
 
-
+/**
+ * Download a JSON file from the provided url and save it to the provided path
+ * @param {String} url The URL from which to download the JSON file 
+ * @param {String} path The filepath to write the downloaded JSON file to
+ * @returns The content of the downloaded JSON file as JSON
+ */
 const downloadJSONFile = (async (url: string, path: string) => {
     const response = await axios.get(url);
     fs.writeFile(path, JSON.stringify(response.data), err => err && console.error(err))
