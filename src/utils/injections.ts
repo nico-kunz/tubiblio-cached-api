253bbaf5c579cde67e5c2d593b67dee84b38fbaa
nkunz <nicokunznk00@gmail.com>
2022-05-10 13:30:46 +0200

    Add methods for managing injections

---
+++ src/utils/injections.ts
@@ -0,0 +1,37 @@
+import fs from 'fs';
+
+type InjectionType = {eprintid: number, official_url: string}
+let injections: Array<InjectionType> = [];
+let injectionsLoaded: boolean = false;
+
+const loadInjections = async () => {
+    return fs.promises.readFile('injections.json').then(data => JSON.parse(data.toString())).then(data => {injections = data; return data});
+}
+
+export const getInjections = async () => {
+    if (injectionsLoaded)
+        return injections
+    else {
+        const data = await loadInjections();
+        injectionsLoaded = true;
+        return data as Array<InjectionType>; 
+    } 
+}
+
+
+export const addInjection = async (injection: InjectionType) => {
+    let data = await getInjections();
+    
+    const i = data.findIndex(elem => elem.eprintid === injection.eprintid);
+    
+    if(i != -1) 
+        data[i] = injection;
+    else 
+        [...data, injection]
+    
+    injections = data;
+    fs.promises.writeFile('injections.json', JSON.stringify(data));
+}
+
+
+
