0e63cadf4655db3b347f727ea37a18296e0c6e3a
nkunz <nicokunznk00@gmail.com>
2022-05-23 02:35:20 +0200

    Refactor injections.ts and add documentation

--- src/utils/injections.ts
+++ src/utils/injections.ts
@@ -1,16 +1,26 @@
 import fs from 'fs';
 
-type InjectionType = {eprintid: number, official_url: string}
+type InjectionType = { eprintid: number, official_url: string }
 let injections: Array<InjectionType> = [];
 let injectionsLoaded: boolean = false;
 
+/** 
+ * Get all injections from the injections.json file and load them into the injections array
+ * @returns The loaded injections as an array of InjectionType
+*/
 const loadInjections = async () => {
-    return fs.promises.readFile('injections.json').then(data => JSON.parse(data.toString())).then(data => {injections = data; return data});
+    let data = await fs.promises.readFile('injections.json');
+    injections = JSON.parse(data.toString());
+    return injections;
 }
 
+/**
+ * Get all injections, if they haven't been loaded yet, load them
+ * @returns All injections as an array of InjectionType
+ */
 export const getInjections = async () => {
     if (injectionsLoaded)
-        return injections
+        return injections;
     else {
         const data = await loadInjections();
         injectionsLoaded = true;
@@ -18,7 +28,9 @@ export const getInjections = async () => {
     } 
 }
 
-
+/**
+ * Add an injection to the injections.json file and the injections array
+ */
 export const addInjection = async (injection: InjectionType) => {
     let data = await getInjections();
     
@@ -27,18 +39,22 @@ export const addInjection = async (injection: InjectionType) => {
     if(i != -1) 
         data[i] = injection;
     else 
-        data = [...data, injection]
+        data = [...data, injection];
 
     injections = data;
     fs.promises.writeFile('injections.json', JSON.stringify(data));
 }
 
+/**
+ * Remove an injection from the injections.json file and the injections array
+ * @param eprintId The eprintId of the injection to remove
+ */
 export const removeInjection = async (eprintId: number) => {
     let data = await getInjections();
     const i = data.findIndex(elem => elem.eprintid === eprintId);
 
     if(i === -1)
-        return
+        return;
     else
         data.splice(i, 1);
 
@@ -46,6 +62,11 @@ export const removeInjection = async (eprintId: number) => {
     fs.promises.writeFile('injections.json', JSON.stringify(data));
 }
 
+/**
+ * Directly inject all injections into the given data array
+ * @param data An array of data to inject into
+ * @returns The given data array with the injected data
+ */
 export const injectData = async (data: any[]) => {
     const inj = await getInjections();
     const injectedData = data.map(dataElem => {
@@ -58,5 +79,3 @@ export const injectData = async (data: any[]) => {
 
     return injectedData;
 }
-
-
